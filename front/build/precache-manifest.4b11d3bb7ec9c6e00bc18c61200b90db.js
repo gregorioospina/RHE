fc8235ce7faca862d4c469dc53ab593874e940f9
gregorioospina <g.ospinaa@uniandes.edu.co>
2019-09-16 22:48:32 -0500

    Deployment

---
+++ front/build/precache-manifest.4b11d3bb7ec9c6e00bc18c61200b90db.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "34526adfc4af34a82ca8e60f03df5535",
+    "url": "/index.html"
+  },
+  {
+    "revision": "be32a3ecc6e243ce8605",
+    "url": "/static/css/main.48ee50a5.chunk.css"
+  },
+  {
+    "revision": "acd3f51bca37aebce493",
+    "url": "/static/js/2.18a2e657.chunk.js"
+  },
+  {
+    "revision": "be32a3ecc6e243ce8605",
+    "url": "/static/js/main.19e64df1.chunk.js"
+  },
+  {
+    "revision": "9ba826fd182ef2cb5489",
+    "url": "/static/js/runtime~main.cef8c144.js"
+  }
+]);
